89e5ef563a9c84dd8b9156e13117cc2ca6446afa
Plamen Petrov <biznetmen@gmail.com>
2025-05-28 10:09:15 +0300

    simple chat added

--- app.py
+++ app.py
@@ -10,33 +10,45 @@ import requests
 
 load_dotenv()
 
-user_input = input("Ask your question here : ")
+history = []
 
-api_key = os.getenv('API_KEY')
+while True:
 
-if not api_key:
-    raise ValueError("API_KEY not found in environment variables")
+    user_input = input("Ask your question here: ")
 
-API_URL = "https://api.openai.com/v1/responses"
+    if user_input == "":
+        print("Exiting the program.")
+        break
 
-KEY = f'Bearer {api_key}'
+    history.append({"role": "user", "content": user_input})
 
-# print(KEY)
+    api_key = os.getenv('API_KEY')
 
-headers = {
-    "Content-Type": "application/json",
-    "Authorization": KEY
-}
+    if not api_key:
+        raise ValueError("API_KEY not found in environment variables")
 
-body = {
-    "model": "gpt-4o-mini",
-    "instructions": "You are a helpful assistant.",
-    "input": user_input
-}
+    API_URL = "https://api.openai.com/v1/responses"
 
-response = requests.post(API_URL, headers=headers, json=body, timeout=10)
+    KEY = f'Bearer {api_key}'
 
-response_data = response.json()
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": KEY
+    }
 
-text = response_data["output"][0]["content"][0]['text']
-print(text)
+    body = {
+        "model": "gpt-4o-mini",
+        "instructions": "You are a helpful assistant.",
+        "input": history
+    }
+
+    response = requests.post(API_URL, headers=headers, json=body, timeout=10)
+
+    response_data = response.json()
+
+    text = response_data["output"][0]["content"][0]['text']
+    
+    print(text)
+
+    history.append({"role": "assistant", "content": text})
+    
